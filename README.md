8d325915b0cb5c3587a83a361fc418642f1aafff
Aytixel <aytixel@gmail.com>
2022-08-01 03:59:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Freenect2cam
+
+Linux Kinect V2 driver, using Freenect2 and OpenCV.
+
+## Build
+
+```
+mkdir build && cd build
+cmake -Dfreenect2_DIR=$HOME/freenect2/lib/cmake/freenect2 .. && make
+```
+
+## Utility command
+
+```
+v4l2-ctl --device=/dev/video0 --all
+v4l2-ctl --list-devices
+
+sudo modprobe v4l2loopback video_nr=0 card_label="Kinect V2 Color" exclusive_caps=1
+
+ffmpeg -f x11grab -framerate 60 -video_size 1920x1080 -i :1.0 -pix_fmt yuv420p -f v4l2 /dev/video0
+ffplay /dev/video0
+
+sudo modprobe -r v4l2loopback
+```
---
+++ src/main.cpp
@@ -0,0 +1,91 @@
+#include <libfreenect2/libfreenect2.hpp>
+#include <libfreenect2/frame_listener_impl.h>
+#include <libfreenect2/packet_pipeline.h>
+
+#include <opencv2/opencv.hpp>
+
+#include <linux/videodev2.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <cstring>
+#include <assert.h>
+
+#define VIDEO_DEVICE "/dev/video0"
+#define FRAME_FORMAT V4L2_PIX_FMT_YUV420
+#define VIDEO_WIDTH 1920
+#define VIDEO_HEIGHT 1080
+#define BYTES_PER_PIXEL 1.5f
+
+using namespace cv;
+
+int main() {
+    libfreenect2::Freenect2 freenect2;
+    libfreenect2::Freenect2Device *dev = 0;
+    libfreenect2::PacketPipeline *pipeline = 0;
+
+    assert(freenect2.enumerateDevices() != 0);
+
+    pipeline = new libfreenect2::OpenGLPacketPipeline();
+    dev = freenect2.openDevice(freenect2.getDefaultDeviceSerialNumber(), pipeline);
+
+    libfreenect2::SyncMultiFrameListener listener(libfreenect2::Frame::Color);
+    libfreenect2::FrameMap frames;
+
+    dev->setColorFrameListener(&listener);
+    dev->start();
+
+    int fd = open(VIDEO_DEVICE, O_RDWR);
+
+	assert(fd >= 0);
+
+	struct v4l2_format vid_format;
+
+	memset(&vid_format, 0, sizeof(vid_format));
+
+	vid_format.type = V4L2_BUF_TYPE_VIDEO_OUTPUT;
+	vid_format.fmt.pix.width = VIDEO_WIDTH;
+	vid_format.fmt.pix.height = VIDEO_HEIGHT;
+	vid_format.fmt.pix.pixelformat = FRAME_FORMAT;
+	vid_format.fmt.pix.sizeimage = BYTES_PER_PIXEL * VIDEO_WIDTH * VIDEO_HEIGHT;
+	vid_format.fmt.pix.field = V4L2_FIELD_NONE;
+	vid_format.fmt.pix.bytesperline = BYTES_PER_PIXEL * VIDEO_WIDTH;
+	vid_format.fmt.pix.colorspace = V4L2_COLORSPACE_SRGB;
+
+	assert(ioctl(fd, VIDIOC_S_FMT, &vid_format) != -1);
+
+    struct v4l2_streamparm vid_parm;
+
+    vid_parm.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    vid_parm.parm.capture.timeperframe.numerator = 1;
+    vid_parm.parm.capture.timeperframe.denominator = 60;
+
+    assert(ioctl(fd, VIDIOC_S_PARM, &vid_parm) != -1);
+
+    unsigned char* dest = (unsigned char*)malloc(BYTES_PER_PIXEL * VIDEO_WIDTH * VIDEO_HEIGHT);
+
+    while(true) {
+        listener.waitForNewFrame(frames);
+        
+        libfreenect2::Frame *rgb = frames[libfreenect2::Frame::Color];
+
+        Mat mrgb(VIDEO_HEIGHT, VIDEO_WIDTH, CV_8UC4, rgb->data);
+        Mat myuv(BYTES_PER_PIXEL * VIDEO_HEIGHT, VIDEO_WIDTH, CV_8UC1, dest);
+        
+        cvtColor(mrgb, myuv, COLOR_RGBA2YUV_YV12);
+        write(fd, dest, BYTES_PER_PIXEL * VIDEO_WIDTH * VIDEO_HEIGHT);
+
+        listener.release(frames);
+    }
+
+    free(dest);
+
+    dev->stop();
+    dev->close();
+    
+    close(fd);
+
+    return 0;
+}
