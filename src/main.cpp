020a43bb6e04cbaee963a7e19120b0931bfcd563
Aytixel <aytixel@gmail.com>
2022-08-07 22:15:19 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <libfreenect2/libfreenect2.hpp>
 #include <libfreenect2/frame_listener_impl.h>
+#include <libfreenect2/logger.h>
 
 #include <opencv2/opencv.hpp>
 
@@ -9,11 +10,8 @@
 
 #include <fcntl.h>
 #include <stdio.h>
-#include <cstring>
-#include <assert.h>
 #include <csignal>
 
-#define VIDEO_DEVICE "/dev/video0"
 #define FRAME_FORMAT V4L2_PIX_FMT_YUV420
 #define VIDEO_WIDTH 1920
 #define VIDEO_HEIGHT 1080
@@ -22,34 +20,48 @@
 using namespace cv;
 using namespace libfreenect2;
 
+char *video_device;
+unsigned char *dest;
+int fd = -1;
 bool running = true;
 
 void handler(int s) {
     running = false;
 }
 
-int main() {
+class CustomFrameListener: public FrameListener {
+    bool onNewFrame(Frame::Type type, Frame *frame) {
+        if (type == Frame::Type::Color) {
+            Mat mrgb(VIDEO_HEIGHT, VIDEO_WIDTH, CV_8UC4, frame->data);
+            Mat myuv(BYTES_PER_PIXEL * VIDEO_HEIGHT, VIDEO_WIDTH, CV_8UC1, dest);
+
+            cvtColor(mrgb, myuv, COLOR_BGR2YUV_I420);
+            write(fd, dest, BYTES_PER_PIXEL * VIDEO_WIDTH * VIDEO_HEIGHT);
+        }
+
+        return false;
+    }
+};
+
+int main(int argc, char *argv[]) {
     signal(SIGINT, handler);
     signal(SIGTERM, handler);
 
-    Freenect2 freenect2;
-    Freenect2Device *dev = 0;
-    PacketPipeline *pipeline = 0;
+    if (argc < 2) {
+        std::cerr << "Not enougth argument! You should enter at least the path to the virtual video device to use. Other argument will be ignored." << std::endl;
+        return -1;
+    }
 
-    assert(freenect2.enumerateDevices() != 0);
+    video_device = *(argv + 1);
+    dest = (unsigned char*)malloc(BYTES_PER_PIXEL * VIDEO_WIDTH * VIDEO_HEIGHT);
 
-    pipeline = new OpenGLPacketPipeline();
-    dev = freenect2.openDevice(freenect2.getDefaultDeviceSerialNumber(), pipeline);
+    // open the virtual video device and configure it
+    fd = open(video_device, O_RDWR);
 
-    SyncMultiFrameListener listener(Frame::Color);
-    FrameMap frames;
-
-    dev->setColorFrameListener(&listener);
-    dev->start();
-
-    int fd = open(VIDEO_DEVICE, O_RDWR);
-
-	assert(fd >= 0);
+    if (fd < 0) {
+        std::cerr << "Can't open virtual video device!" << std::endl;
+        return -1;
+    }
 
 	struct v4l2_format vid_format;
 
@@ -64,38 +76,52 @@ int main() {
 	vid_format.fmt.pix.bytesperline = BYTES_PER_PIXEL * VIDEO_WIDTH;
 	vid_format.fmt.pix.colorspace = V4L2_COLORSPACE_SRGB;
 
-	assert(ioctl(fd, VIDIOC_S_FMT, &vid_format) != -1);
+	if (ioctl(fd, VIDIOC_S_FMT, &vid_format) == -1) {
+        std::cerr << "Can't set virtual video device format!" << std::endl;
+        return -1;
+    }
 
     struct v4l2_streamparm vid_parm;
 
+	memset(&vid_parm, 0, sizeof(vid_parm));
+
     vid_parm.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     vid_parm.parm.capture.timeperframe.numerator = 1;
-    vid_parm.parm.capture.timeperframe.denominator = 60;
+    vid_parm.parm.capture.timeperframe.denominator = 30;
 
-    assert(ioctl(fd, VIDIOC_S_PARM, &vid_parm) != -1);
-
-    unsigned char* dest = (unsigned char*)malloc(BYTES_PER_PIXEL * VIDEO_WIDTH * VIDEO_HEIGHT);
-
-    while(running) {
-        listener.waitForNewFrame(frames);
-        
-        Frame *rgb = frames[Frame::Color];
-
-        Mat mrgb(VIDEO_HEIGHT, VIDEO_WIDTH, CV_8UC4, rgb->data);
-        Mat myuv(BYTES_PER_PIXEL * VIDEO_HEIGHT, VIDEO_WIDTH, CV_8UC1, dest);
-        
-        cvtColor(mrgb, myuv, COLOR_RGBA2YUV_YV12);
-        write(fd, dest, BYTES_PER_PIXEL * VIDEO_WIDTH * VIDEO_HEIGHT);
-
-        listener.release(frames);
+    if (ioctl(fd, VIDIOC_S_PARM, &vid_parm) == -1) {
+        std::cerr << "Can't set virtual video device frame rate!" << std::endl;
+        return -1;
     }
 
-    free(dest);
+    // open the kinect
+    Freenect2 freenect2;
+    Freenect2Device *dev = 0;
+
+    // disable the freenect logger
+    setGlobalLogger(NULL);
+
+    if (freenect2.enumerateDevices() == 0) {
+        std::cerr << "Kinect not connected!" << std::endl;
+        return -1;
+    }
+
+    dev = freenect2.openDevice(freenect2.getDefaultDeviceSerialNumber());
+
+    CustomFrameListener listener;
+
+    dev->setColorFrameListener(&listener);
+    dev->startStreams(true, false);
+
+    while(running) {
+        sleep(1);
+    }
 
     dev->stop();
     dev->close();
     
     close(fd);
+    free(dest);
 
     return 0;
 }
